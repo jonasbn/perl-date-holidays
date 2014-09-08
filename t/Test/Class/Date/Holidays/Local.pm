ac48bbcf00ae48aa5a983e464c48b5f1489e3923
jonasbn <jonasbn@logicLAB.dk>
2014-09-08 20:43:20 +0200

    Added test with more than one countries

--- t/Test/Class/Date/Holidays/Local.pm
+++ t/Test/Class/Date/Holidays/Local.pm
@@ -54,4 +54,22 @@ sub cancelling_christmas : Test(3) {
     is($holiday->{'local'}, '');
 }
 
+sub cancelling_christmas_for_all : Test(4) {
+    my $self = shift;
+
+    $HOLIDAYS_FILE = 't/cancelling_christmas.json';
+
+    ok(my $holiday = $self->{dh}->is_holiday(
+        year      => 2014,
+        month     => 12,
+        day       => 24,
+        countries => ['+local','dk', 'no'],
+    ));
+
+    is($holiday->{'dk'}, '');
+    is($holiday->{'no'}, '');
+    is($holiday->{'local'}, '');
+}
+
+
 1;
