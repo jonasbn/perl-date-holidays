79ae06bb23f46386e7497de43c88e1b1c350799d
jonasbn <jonasbn@gmail.com>
2018-06-12 22:40:06 +0200

    Bumped version number

--- lib/Date/Holidays/Adapter/CA_ES.pm
+++ lib/Date/Holidays/Adapter/CA_ES.pm
@@ -7,7 +7,7 @@ use base qw(Date::Holidays::Adapter::ES);
 
 use vars qw($VERSION);
 
-$VERSION = '1.12';
+$VERSION = '1.13';
 
 1;
 
@@ -21,7 +21,7 @@ Date::Holidays::Adapter::CA_ES - adapter dummy class for Date::Holidays::CA_ES
 
 =head1 VERSION
 
-This POD describes version 1.12 of Date::Holidays::Adapter::CA_ES
+This POD describes version 1.13 of Date::Holidays::Adapter::CA_ES
 
 =head1 DESCRIPTION
 
