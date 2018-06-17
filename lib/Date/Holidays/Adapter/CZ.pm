7edb8e1ec8b86966c65d9b58af2cc6422da91ebe
jonasbn <jonasbn@gmail.com>
2018-06-17 09:21:26 +0200

    Bumped version number

--- lib/Date/Holidays/Adapter/CZ.pm
+++ lib/Date/Holidays/Adapter/CZ.pm
@@ -7,7 +7,7 @@ use base 'Date::Holidays::Adapter';
 
 use vars qw($VERSION);
 
-$VERSION = '1.15';
+$VERSION = '1.16';
 
 my $format = '%#:%m%d';
 
@@ -87,7 +87,7 @@ Date::Holidays::Adapter::CZ - an adapter class for Date::Holidays::CZ
 
 =head1 VERSION
 
-This POD describes version 1.15 of Date::Holidays::Adapter::CZ
+This POD describes version 1.16 of Date::Holidays::Adapter::CZ
 
 =head1 DESCRIPTION
 
