c78b8cfeed32333923504995bcb9a31f6216a533
jonasbn <jonasbn@gmail.com>
2023-08-09 21:11:31 +0200

    Added some more of my basic prototypes

---
+++ prototypes/ro.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib;$FindBin::Bin/lib";
+
+use Date::Holidays;
+use Data::Dumper;
+
+my $dh = Date::Holidays->new( countrycode => 'ro' );
+my $holidays = $dh->holidays( year => 2023 );
+
+print Dumper $holidays;
+
+exit 0;
